8ddab23b5bef86409f3168661a6b2e0d6bf14c82
Raviraj <tbs.raviraj@gmail.com>
2020-03-20 11:49:36 +0530

    initial commit

---
+++ Architecture.Utility/JsonSerializeDeserializer.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Architecture.Utility
+{
+    public static class JsonSerializeDeserializer
+    {
+        public static TEntity JSONStringToObject<TEntity>(string JsonStr)
+        {
+            return JsonConvert.DeserializeObject<TEntity>(JsonStr);
+        }
+
+        public static string JsonString<TEntity>(TEntity obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
